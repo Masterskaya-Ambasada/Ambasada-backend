34fea4cda5cb397a245ed5be0ab014fb0f0fb326
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2026-04-06 22:15:20 +0300

    feat-8: complete django init with custom user and clean linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,6 @@ except FileNotFoundError:
     print('Error: .env file not found!', file=sys.stderr)
     sys.exit(1)
 
-
 # Security
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -41,6 +40,9 @@ INSTALLED_APPS = [
     'django_filters',
     'django_extensions',
     'drf_spectacular',
+    'projects',
+    'users',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,8 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'users.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -138,6 +142,10 @@ _COLLECTSTATIC_DRYRUN = config(
     default=False,
 )
 STATIC_ROOT = '.static' if _COLLECTSTATIC_DRYRUN else '/var/www/django/static'
+
+# Media files (User uploaded content)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # REST Framework
 REST_FRAMEWORK = {
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Подключаем пути будущих приложений (projects, api, users)
+    path('api/', include('api.urls')),
 ]
+
+# Раздача медиа-файлов (фото проектов) в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
