47c4525fc6cda62030ac0f345913bd697f50fd21
Andrey Golovushkin <frenkodetto@yandex.ru>
2026-04-08 17:34:44 +0300

    feat_2-projects(api): add public projects endpoints with filters and pagination

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,26 @@
+"""Маршруты API приложения."""
+
+from django.urls import path
+from projects.views import (
+    ProjectDetailView,
+    ProjectListView,
+    ProjectTagListView,
+    ProjectTypeListView,
+)
+
 app_name = 'api'
 
-urlpatterns = []
+urlpatterns = [
+    path('v1/projects', ProjectListView.as_view(), name='projects-list'),
+    path('v1/projects/tags', ProjectTagListView.as_view(), name='projects-tags'),
+    path(
+        'v1/projects/types',
+        ProjectTypeListView.as_view(),
+        name='projects-types',
+    ),
+    path(
+        'v1/projects/<slug:project_id>',
+        ProjectDetailView.as_view(),
+        name='projects-detail',
+    ),
+]
