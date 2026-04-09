27736f417587ee5f03cb84ba85b3c3bbb6ba0eec
Andrey Golovushkin <frenkodetto@yandex.ru>
2026-04-09 15:43:53 +0300

    fix for tests

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -213,21 +213,13 @@ class ProjectContentBlock(models.Model):
         except ValidationError as error:
             raise ValidationError({'string_list': error.messages}) from error
         if self.variant == self.Variant.IMAGE_WITH_LIST and not self.string_list:
-            raise ValidationError(
-                {'string_list': _('Для варианта 1 требуется хотя бы один элемент списка.')}
-            )
+            raise ValidationError({'string_list': _('Для варианта 1 требуется хотя бы один элемент списка.')})
         if self.variant != self.Variant.IMAGE_WITH_LIST and self.string_list:
-            raise ValidationError(
-                {'string_list': _('Список тезисов допустим только для варианта 1.')}
-            )
+            raise ValidationError({'string_list': _('Список тезисов допустим только для варианта 1.')})
         if self.variant == self.Variant.TWO_IMAGES and not self.left_image:
-            raise ValidationError(
-                {'left_image': _('Для варианта 2 требуется второе изображение.')}
-            )
+            raise ValidationError({'left_image': _('Для варианта 2 требуется второе изображение.')})
         if self.variant != self.Variant.TWO_IMAGES and self.left_image:
-            raise ValidationError(
-                {'left_image': _('Второе изображение допустимо только для варианта 2.')}
-            )
+            raise ValidationError({'left_image': _('Второе изображение допустимо только для варианта 2.')})
 
     def save(self, *args, **kwargs):
         """Автоматически назначает порядок блока в пределах проекта."""
