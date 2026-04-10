01e046b410734f8ac939c4897ae3a7d998f68a30
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2026-04-10 12:46:54 +0300

    Добавлена Swagger-документация (drf-spectacular)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,16 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 app_name = 'api'
 
-urlpatterns = []
+doc_urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='api:schema'), name='swagger'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='api:schema'), name='redoc'),
+]
+
+urlpatterns = [] + doc_urlpatterns
