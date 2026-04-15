2efde9411da8136cba047c8caec394ef573d95a4
Lera Lugovina <valug48@gmail.com>
2026-04-15 18:16:40 +0300

    add model

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from contacts.constants import MAX_NAME_LENGTH, MAX_REASON_LENGTH
+
+
+class ContactRequest(models.Model):
+    '''Модель для формы обратной связи'''
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    email = models.EmailField()
+    message = models.TextField()
+    reason = models.CharField(max_length=MAX_REASON_LENGTH)
+
+    def __str__(self):
+        return f'{self.name} <{self.email}> - {self.reason}'
