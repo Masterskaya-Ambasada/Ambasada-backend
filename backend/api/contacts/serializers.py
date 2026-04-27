0d0f80dd5b962baec2927f0a1ce21fae1ddd67b3
Lera Lugovina <valug48@gmail.com>
2026-04-27 19:36:51 +0300

    add new serializers

--- backend/api/contacts/serializers.py
+++ backend/api/contacts/serializers.py
@@ -1,12 +1,32 @@
-from contacts.models import ContactRequest
+from contacts.models import ContactPageContent, ContactRequest, ContactSocialLink
 from rest_framework import serializers
 
 
 class ContactRequestSerializer(serializers.ModelSerializer):
-    """Сериалайзер для формы обратной связи со встроенным антиспамом."""
+    '''Сериалайзер для формы обратной связи со встроенным антиспамом.'''
 
     contact_preference = serializers.CharField(required=False, allow_blank=True, write_only=True)
 
     class Meta:
         model = ContactRequest
         fields = ['name', 'email', 'message', 'reason', 'contact_preference']
+
+    def create(self, validated_data):
+        validated_data.pop('contact_preference', None)
+        return super().create(validated_data)
+
+
+class ContactSocialLinkSerializer(serializers.ModelSerializer):
+    '''Сериалайзер для для ссылок на соцсети и мессенджеры.'''
+
+    class Meta:
+        model = ContactSocialLink
+        fields = ('social_type', 'url', 'order')
+
+
+class ContactPageContentSerializer(serializers.ModelSerializer):
+    '''Сериалайзер для редактируемой ссылки на пожертвования.'''
+
+    class Meta:
+        model = ContactPageContent
+        fields = ('donation_text',)
