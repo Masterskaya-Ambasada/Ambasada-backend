3e88718f4699e76b7ad579b238a1051955221aac
Liudmila <bauklu@mail.ru>
2026-05-07 22:19:51 +0300

    move cookie defaults to constants

--- backend/site_config/constants.py
+++ backend/site_config/constants.py
@@ -1,1 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+
 CACHE_KEY_INIT = 'site_config_init'
+COOKIE_BUTTON_TEXT_MAX_LENGTH = 20
+DEFAULT_COOKIE_BUTTON_TEXT = 'OK'
+DEFAULT_COOKIE_MESSAGE = _(
+    'Мы используем технические cookie для корректной работы сайта.'
+)
