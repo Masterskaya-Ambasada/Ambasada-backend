233844004beb432479623f8527a56752432c7ef7
Liudmila <bauklu@mail.ru>
2026-05-07 12:29:35 +0300

    add cookie fields to translation options

--- backend/site_config/translation.py
+++ backend/site_config/translation.py
@@ -5,4 +5,10 @@ from .models import SiteConfig
 
 @register(SiteConfig)
 class SiteConfigTranslationOptions(TranslationOptions):
-    fields = ('site_name', 'seo_description', 'copyright', 'privacy_policy')
+    fields = (
+        'site_name',
+        'seo_description',
+        'copyright',
+        'privacy_policy',
+        'cookie_message',
+    )
