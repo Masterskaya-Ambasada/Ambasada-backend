cee0c44f82003a40c137cd75af5084ce5812dda2
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2026-05-10 22:49:12 +0300

    Убрал лишние комментарии

--- backend/site_config/signals.py
+++ backend/site_config/signals.py
@@ -1,5 +1,5 @@
-from contacts.models import ContactSocialLink  # Твоя модель соцсетей
-from django.db.models.signals import post_delete, post_save  # Добавили удаление
+from contacts.models import ContactSocialLink
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from site_config.cache import clear_config_cache
