e47614a1c39285c473190e0bf5778a3574ba9f76
Andrey Golovushkin <frenkodetto@yandex.ru>
2026-05-20 11:35:56 +0300

    fix(projects): rename detail path parameter to project slug

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -64,7 +64,7 @@ project_urlpatterns = [
     path('', ProjectListView.as_view(), name='projects-list'),
     path('tags/', ProjectTagListView.as_view(), name='projects-tags'),
     path('types/', ProjectTypeListView.as_view(), name='projects-types'),
-    path('<slug:project_id>/', ProjectDetailView.as_view(), name='projects-detail'),
+    path('<slug:project_slug>/', ProjectDetailView.as_view(), name='projects-detail'),
 ]
 
 # Эндпоинты страницы "О нас"
