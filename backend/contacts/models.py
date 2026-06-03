f54278c920d1979f0e29600e7985bfd4b23e681a
Lera Lugovina <valug48@gmail.com>
2026-06-03 21:38:53 +0300

    fix formatting

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -4,12 +4,12 @@ from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 from contacts.constants import (
+    DEFAULT_SOCIAL_ORDER,
+    DONATION_TEXT_PREVIEW_LENGTH,
     MAX_MESSAGE_LENGTH,
     MAX_NAME_LENGTH,
     MAX_REASON_LENGTH,
     SOCIAL_TYPE_MAX_LENGTH,
-    DEFAULT_SOCIAL_ORDER,
-    DONATION_TEXT_PREVIEW_LENGTH
 )
 
 
